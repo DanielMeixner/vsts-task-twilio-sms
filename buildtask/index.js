0d6550cecd6b74112e7979a5011e52b5d0c31ee7
DanielMeixner <daniel.meixner@live.com>
2017-04-12 16:24:20 +0200

    gulped

--- buildtask/index.js
+++ buildtask/index.js
@@ -1,30 +1,23 @@
 'use strict';
 
-
 var Client = require('azure-iothub').Client;
 var Message = require('azure-iot-common').Message;
 var req = require("request");
 var taaa = require('vsts-task-lib/task');
-// var connectionString = process.argv[2];
-// var iothuburl = process.argv[3];
-// var authb64 = process.argv[4];
-
 
 var connectionString = taaa.getInput('IotHubConnectionString');
 var iothuburl = taaa.getInput('IotHubDeviceRequestUrl');
 var authb64 = taaa.getInput('IoTHubAuthString');
-
+var cloudToDeviceMsg = taaa.getInput('MessageContent');
+var messageId =taaa.getInput('MessageId');
 
 var buf = Buffer.from(authb64, 'base64');
 var auth = buf.toString('utf8');
 
-    
 console.log("connectionString:" + connectionString);
 console.log("iothuburl:" + iothuburl);
 console.log("decrypted Auth: " + auth);
 
-      
-console.log("LOG1");
 var headers = {
     'Content-Type': 'application/json',
     'Authorization': auth
@@ -35,64 +28,63 @@ var options = {
     'url': iothuburl,
     'method': 'GET',
     'headers': headers
-
 };
-console.log("start device query LOG3");
 
 var deviceList;
-
+var nrOfDevices = 0;
+var nrOfMsgsTriggered = 0;
 
 // query devices from iot hub
 req(options, function (error, response, body) {
     console.log("inside device query Log3b");
     console.log(body);
     deviceList = JSON.parse(body);
+    nrOfDevices = deviceList.length;
+    console.log("Found " + nrOfDevices + " devices.");
 });
-
-
-console.log("LOG4");
-
 
 var SendNotification = function (devId) {
     console.log(devId);
 }
-console.log("LOG5");
 
 var serviceClient = Client.fromConnectionString(connectionString);
-console.log("LOG6");
 
 function printResultFor(op) {
     return function printResult(err, res) {
-        if (err) console.log(op + ' error: ' + err.toString());
-        if (res) console.log(op + ' status: ' + res.constructor.name);
+        if (err) {
+            console.log(op + ' error: ' + err.toString());
+            if (nrOfDevices == ++nrOfMsgsTriggered) {
+                console.log("Exiting gracefully after delivery.");
+                process.exit();
+            }
+        }
+        if (res) {
+            console.log(op + ' status: ' + res.constructor.name);
+            if (nrOfDevices == ++nrOfMsgsTriggered) {
+                console.log("Exiting gracefully after delivery.");
+                process.exit();
+            }
+        }
     };
 }
-console.log("LO7");
 
 function receiveFeedback(err, receiver) {
     receiver.on('message', function (msg) {
         console.log('Feedback message:')
         console.log(msg.getData().toString('utf-8'));
-        process.exit();
     });
 }
-console.log("LOG8");
-
-
 
 serviceClient.open(function (err) {
-
-    console.log("LOG 9 open service client");
     if (err) {
         console.error('Could not connect: ' + err.message);
     } else {
         console.log('Service client connected');
 
         serviceClient.getFeedbackReceiver(receiveFeedback);
-
-        var message = new Message('Cloud to device message. From VSO.');
+        var message = new Message(cloudToDeviceMsg);
         message.ack = 'full';
-        message.messageId = "My Message to all devices";
+        message.messageId = messageId;
         console.log('Sending message: ' + message.getData());
         deviceList.forEach(function (element) {
             var id = element.deviceId;
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var util = require('gulp-util');
+var shell = require('gulp-shell')
+
+var replace = require('gulp-replace');
+ 
+
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('build',['copy','version','vsts-build-extension'])
+{
+
+}
+
+gulp.task('vsts-build-extension',['version'], function(){
+    return gulp.src('*.js', {read: false})
+  .pipe(shell('tfx extension create --manifest-globs vss-extension.json'
+,
+{cwd:"_out"}
+))
+    
+});
+
+gulp.task('copy', function()
+{
+     gulp.src(['images/**/*']).pipe(gulp.dest('_out/images'));
+     gulp.src(['buildtask/node_modules/*']).pipe(gulp.dest('_out/buildtask/node_modules'));
+});
+
+
+gulp.task('version',['copy'], function(){
+  gulp.src(['buildtask/*'])
+    .pipe(replace('G_VERSION', '0.0.'+util.env.num))
+    .pipe(gulp.dest('_out/buildtask'));
+  gulp.src(['vss-extension.json'])
+    .pipe(replace('G_VERSION', '0.0.'+util.env.num))
+    .pipe(gulp.dest('_out/'));
+    
+});
